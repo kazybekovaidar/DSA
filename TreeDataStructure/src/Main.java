d25c1482fd90025f84e4bbf3173bcbf1299a56f9
Aidar Kazybekov <kazybekov_a@auca.kg>
2022-08-07 23:44:40 +0600

    Binary search tree introduction

---
+++ TreeDataStructure/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    class Node {
+        Node left, right;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        //Insert Method
+        public void insert(int value) {
+            if (value <= data) {
+                if (left == null) {
+                    left = new Node(value);
+                } else {
+                    left.insert(value);
+                }
+            } else {
+                if (right == null) {
+                    right = new Node(value);
+                } else {
+                    right.insert(value);
+                }
+            }
+        }
+
+        public boolean contains(int value) {
+            if (value == data) {
+                return true;
+            }
+            if (value < data) {
+                if (left == null) {
+                    return false;
+                } else {
+                    return left.contains(value);
+                }
+            } else {
+                if (right == null) {
+                    return false;
+                } else {
+                    return right.contains(value);
+                }
+            }
+        }
+
+        public void printInOrder() {
+            if (left != null) {
+                left.printInOrder();
+            }
+            System.out.println(data);
+            if (right != null) {
+                right.printInOrder();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
