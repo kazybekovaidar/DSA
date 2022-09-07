79ebcefc309ff1811cd596ec8009e7365d81839b
Aidar Kazybekov <kazybekov_a@auca.kg>
2022-09-07 23:25:29 +0600

    wrong answer is Anagram

---
+++ LeetCode75/src/isAnagram.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Scanner;
+
+public class isAnagram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        String t = scan.next();
+        System.out.println(arrangeAlp(s) + "\n" + arrangeAlp(t));
+    }
+
+    public static String arrangeAlp(String s){
+        char[] arr = new char[26];
+        for (int i = 0; i < s.length(); i++) {
+            int ind = s.charAt(i)-97;
+            arr[ind] = s.charAt(i);
+        }
+        StringBuilder str = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>=97 && arr[i]<=122){
+                str.append(arr[i]);
+            }
+        }
+        return str.toString();
+    }
+}
